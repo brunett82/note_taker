7e218af09995a3dce82e82f33c3c5fccb0fafad5
Rob Brunett <rb3@Robs-Air.attlocal.net>
2021-06-09 19:46:39 -0400

    dependencies and express setup

---
+++ server.js
@@ -0,0 +1,8 @@
+//dependencies
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+
+//setup express
+const app = express();
+const PORT = process.env.PORT || 4000
