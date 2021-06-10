69c2e00089d94854a420fd824b6b01a82d0b11b4
Rob Brunett <rb3@Robs-Air.attlocal.net>
2021-06-09 20:25:54 -0400

    initial server setup

--- server.js
+++ server.js
@@ -6,3 +6,14 @@ const express = require('express');
 //setup express
 const app = express();
 const PORT = process.env.PORT || 4000
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+require("./routes/htmlroutes")(app);
+require("./routes/apiRoutes")(app);
+
+app.listen(PORT, function(){
+    console.log(`Using PORT: ${PORT}`);
+})
