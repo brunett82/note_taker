0ca7106a615c56fd3a611a280a3f968dee6db2e6
Rob Brunett <rb3@Robs-Air.attlocal.net>
2021-06-13 17:53:05 -0400

    reworked server and routes, success!

--- server.js
+++ server.js
@@ -1,17 +1,81 @@
-//dependencies
+//Dependencies
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
-//setup express
-const app = express();
-const PORT = process.env.PORT || 3000;
+// Express Setup
+let app = express();
+let PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({extended: true}));
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+let notes = require("./db/db.json");
 
-require("./routes/htmlroutes")(app);
-require("./routes/apiRoutes")(app);
 
-app.listen(PORT, function() {
-    console.log(`Using PORT: ${PORT}`);
-})
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// Api to display notes
+app.get("/api/notes", function (req, res) {
+  fs.readFile("db/db.json", "utf8", function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.json(notes);
+  });
+});
+
+// Start Server
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
+
+// Create new note
+app.post("/api/notes", function (req, res) {
+  let variable = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+  let id = variable + Date.now();
+  let newN = {
+    id: id,
+    title: req.body.title,
+    text: req.body.text,
+  };
+  console.log(typeof notes);
+  notes.push(newN);
+  const stringifyNote = JSON.stringify(notes);
+  res.json(notes);
+  fs.writeFile("db/db.json", stringifyNote, (err) => {
+    if (err) console.log(err);
+    else {
+      console.log("Note successfully saved");
+    }
+  });
+});
+
+//Delete note
+app.delete("/api/notes/:id", function (req, res) {
+  let nId = req.params.id;
+  fs.readFile("db/db.json", "utf8", function (err, data) {
+    let updatedNotes = JSON.parse(data).filter((note) => {
+      console.log("note.id", note.id);
+      console.log("New ID", nId);
+      return note.id !== nId;
+    });
+    notes = updatedNotes;
+    const stringifyNote = JSON.stringify(updatedNotes);
+    fs.writeFile("db/db.json", stringifyNote, (err) => {
+      if (err) console.log(err);
+      else {
+        console.log("Delete Successful");
+      }
+    });
+    res.json(stringifyNote);
+  });
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
