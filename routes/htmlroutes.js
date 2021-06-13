3b3e9f7202af31a2db285bd65260f0b5c29e06b4
Rob Brunett <rb3@Robs-Air.attlocal.net>
2021-06-13 15:56:53 -0400

    route update

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -4,7 +4,7 @@ module.exports = app => {
     app.get("/notes", function(req, resp) {
         resp.sendFile(path.join(__dirname, "../public/notes.html"));
     });
-    app.get("/", function(req, resp) {
+    app.get("*", function(req, resp) {
         resp.sendFile(path.join(__dirname, "../public/index.html"));
     })
 }
